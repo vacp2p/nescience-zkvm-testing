be174dee80939018ab81ba49037e98cf00fa993a
jonesmarvin8 <83104039+jonesmarvin8@users.noreply.github.com>
2025-07-09 12:58:47 -0400

    updates

--- nexus/ped_vesta_test/src/guest/src/main.rs
+++ nexus/ped_vesta_test/src/guest/src/main.rs
@@ -1,19 +1,19 @@
 #![cfg_attr(target_arch = "riscv32", no_std, no_main)]
-use pasta_curves::group::Group;
+//use pasta_curves::group::Group;
 
 #[nexus_rt::main]
 fn main() {
-    let g1 = pasta_curves::vesta::Point::generator();
-    let g2 = g1 + g1;
-    let g3 = g1 + g2;
-    let g4 = g1 + g3;
-    let g5 = g1 + g4;
+   // let g1 = pasta_curves::vesta::Point::generator();
+   // let g2 = g1 + g1;
+   // let g3 = g1 + g2;
+   // let g4 = g1 + g3;
+   // let g5 = g1 + g4;
         
-    let s1 = pasta_curves::vesta::Scalar::from(87329482u64);
-    let s2 = pasta_curves::vesta::Scalar::from(37264829u64);
-    let s3 = pasta_curves::vesta::Scalar::from(98098098u64);
-    let s4 = pasta_curves::vesta::Scalar::from(63980948u64);
-    let s5 = pasta_curves::vesta::Scalar::from(15098098u64);
+    //let s1 = pasta_curves::vesta::Scalar::from(87329482u64);
+    //let s2 = pasta_curves::vesta::Scalar::from(37264829u64);
+    //let s3 = pasta_curves::vesta::Scalar::from(98098098u64);
+    //let s4 = pasta_curves::vesta::Scalar::from(63980948u64);
+    //let s5 = pasta_curves::vesta::Scalar::from(15098098u64);
 
-    let _ = g1*s1 + g2*s2 + g3*s3 + g4*s4 + g5*s5;
+   // let _ = g1*s1 + g2*s2 + g3*s3 + g4*s4 + g5*s5;
 }
