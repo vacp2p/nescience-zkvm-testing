513282cb487d0ec9c3bcda843efce367fedd5f91
Sergio Chouhy <sergio.chouhy@gmail.com>
2025-07-24 12:17:50 -0300

    add fix

--- risc0/single_hash/methods/guest/src/main.rs
+++ risc0/single_hash/methods/guest/src/main.rs
@@ -1,10 +1,9 @@
+use k256::elliptic_curve::group::GroupEncoding;
 use risc0_zkvm::{
     guest::env,
     sha::{Impl, Sha256},
 };
 use zerocopy::IntoBytes;
-use k256::elliptic_curve::group::GroupEncoding;
-
 
 fn main() {
     // read the input
@@ -21,20 +20,17 @@ fn main() {
 
     //version
     //owner
-    let mut array2 = [owner.to_bytes()];
-    let mut array3 = [amount.try_into().unwrap()];
-    //storage
-    let array4 = [  nonce.try_into().unwrap(),
-                    privacy,
-                    const1.try_into().unwrap(),
-                    const2.try_into().unwrap(),];
-    //array2.clone_from_slice(&array3);
-    array3.clone_from_slice(&array4);
+    let mut bytes_to_hash: [u8; 66] = [0; 66];
 
-    //array1.clone_from_slice(&array3);
-    //array1.clone_from_slice(array4);
-                
-    let _hash = Impl::hash_bytes(&array3);
+    let owner_bytes: [u8; 33] = owner.to_bytes().try_into().unwrap();
+    bytes_to_hash[..33].copy_from_slice(&owner_bytes);
+    bytes_to_hash[33..41].copy_from_slice(&amount.to_le_bytes());
+    bytes_to_hash[41..49].copy_from_slice(&nonce.to_le_bytes());
+    bytes_to_hash[49..50].copy_from_slice(&privacy.to_le_bytes());
+    bytes_to_hash[50..58].copy_from_slice(&const1.to_le_bytes());
+    bytes_to_hash[58..].copy_from_slice(&const2.to_le_bytes());
+
+    let _hash = Impl::hash_bytes(&bytes_to_hash);
 
     // write public output to the journal
     env::commit(&input);
